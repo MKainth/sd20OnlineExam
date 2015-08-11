4f32a859e75fe7da25a17b56d790a17ac0b38407
MKainth <mandeep3183@gmail.com>
2015-08-11 16:23:08 -0500

    "add classes"

--- Group_Project_Online_Exam/Student.cs
+++ Group_Project_Online_Exam/Student.cs
@@ -7,5 +7,12 @@ namespace Group_Project_Online_Exam
 {
     public class Student
     {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int ProgramId { get; set; }
+        public int SessionId { get; set; }
     }
 }
--- Group_Project_Online_Exam/Teacher.cs
+++ Group_Project_Online_Exam/Teacher.cs
@@ -7,5 +7,10 @@ namespace Group_Project_Online_Exam
 {
     public class Teacher
     {
+        public int TeacherId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int admin { get; set; }
     }
 }
