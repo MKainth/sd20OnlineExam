dcc275c1d8dede6292732a697484199a163238da
MKainth <mandeep3183@gmail.com>
2015-08-11 15:37:35 -0500

    gfgf

--- Group_Project_Online_Exam/Instal.sql
+++ Group_Project_Online_Exam/Instal.sql
@@ -15,7 +15,7 @@ create table tbSession
 go
 insert into tbSession(SessionCode) values('SD20'),('AS')
 
-	insert into tbSession (SessionCode) values ('SD20'),('AS')
+	--insert into tbSession (SessionCode) values ('SD20'),('AS')
 -----------------------------------------------
 
 create table tbProgram
@@ -27,16 +27,21 @@ create table tbProgram
 go
 
 insert into tbProgram(ProgramName, SessionId)values
-	('Software and Database Developer', '1'),('Accounting Specialist', '2'),('Administrative Professional', '3'),
+	('Software and Database Developer', 1),('Accounting Specialist', 2)/*,('Administrative Professional', '3'),
 	('Business Administration', '4'),('Casino / Resort / Event Coordinator', '5'),('Legal Assistant', '6'),
 	('Travel Counsellor', '7'),('Veterinary Office Assistant', '8'),('Network Engineering', '9'),
 	('Enhanced Health Care Aide', '10'),('Health Care Aide ', '11'),('Massage Therapy ', '12'),
 	('Medical Laboratory Assistant', '13'),('Medical Office Assistant', '14'),('Nursing Assistant', '15'),
-	('Pharmacy Technician', '16')
+	('Pharmacy Technician', '16')*/
 go
 
 -----------------------------------------------
-
+create table tbDifficulty
+(
+	DifficultyId int primary key identity(1,1),
+	Name Varchar(60)
+)
+go
 create table tbTeacher
 (
 	TeacherId int primary key identity(1,1),
@@ -75,22 +80,7 @@ create table tbQuestion
   Marks int
 )
 go
-create table tbQuiz
-(
-  QuizeId int primary key identity(1,1),
-  ProgramId int foreign key references tbProgram(ProgramId),
-  DifficultyId int foreign key references tbDifficulty(DifficultyId)	
-     
-)
-go
 
-create table tbQuizResponse
-(
-  QuizResponseId int primary key identity(1,1),
-  ExamDate Date,
-  StudentId int foreign key references tbStudent(StudentId)	    
-)
-go
 
 -----------------------------------------------
 
@@ -98,7 +88,7 @@ go
 
 create table tbQuiz
 (
-  QuizeId int primary key identity(1,1),
+  QuizId int primary key identity(1,1),
   ProgramId int foreign key references tbProgram(ProgramId),
   DifficultyId int foreign key references tbDifficulty(DifficultyId)	
      
