88a5adc0ec1d42ab2d3275b5503fa23508275ba5
mohammad rahim <mohammad.rahim@robertsoncollege.net>
2015-08-11 15:56:56 -0500

    hgvgh

--- Group_Project_Online_Exam/Instal.sql
+++ Group_Project_Online_Exam/Instal.sql
@@ -13,20 +13,29 @@ create table tbSession
 	SessionCode Varchar(60)
 )
 go
+insert into tbSession(SessionCode) values('SD20'),('AS')
 
-	insert into tbSession (SessionCode) values ('SD20'),('AS')
+	--insert into tbSession (SessionCode) values ('SD20'),('AS')
 -----------------------------------------------
 
 create table tbProgram
 (
 	ProgramId int primary key identity(1,1),
-	ProgramName varchar(60),
+	ProgramName varchar(max),
 	SessionId int foreign key references tbSession(SessionId) 
 )
 go
 
------------------------------------------------
+insert into tbProgram(ProgramName, SessionId)values
+	('Software and Database Developer', 1),('Accounting Specialist', 2)/*,('Administrative Professional', '3'),
+	('Business Administration', '4'),('Casino / Resort / Event Coordinator', '5'),('Legal Assistant', '6'),
+	('Travel Counsellor', '7'),('Veterinary Office Assistant', '8'),('Network Engineering', '9'),
+	('Enhanced Health Care Aide', '10'),('Health Care Aide ', '11'),('Massage Therapy ', '12'),
+	('Medical Laboratory Assistant', '13'),('Medical Office Assistant', '14'),('Nursing Assistant', '15'),
+	('Pharmacy Technician', '16')*/
+go
 
+-----------------------------------------------
 create table tbDifficulty
 (
 	DifficultyId int primary key identity(1,1),
@@ -97,24 +106,27 @@ create table tbQuestion
 )
 go
 
+
 -----------------------------------------------
+
+------------------------------------------------
+
+create table tbQuiz
+(
+  QuizId int primary key identity(1,1),
+  ProgramId int foreign key references tbProgram(ProgramId),
+  DifficultyId int foreign key references tbDifficulty(DifficultyId)	
+     
+)
+go
+
+------------------------------------------------
 
 create table tbQuizResponse
 (
   QuizResponseId int primary key identity(1,1),
   ExamDate Date,
   StudentId int foreign key references tbStudent(StudentId)	    
-)
-go
-
-------------------------------------------------
-
-create table tbQuestionResponse
-(
-  QuestionResponseId int primary key identity(1,1),
-  QuizResponseId int foreign key references tbQuizResponse(QuizResponseId),
-  QuestionId int foreign key references tbQuestion(QuestionId),
-  Response varchar(4)   
 )
 go
 
