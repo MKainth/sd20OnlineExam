6b8dad651dad8e3095eaf652346509b04edef068
mohammad rahim <mohammad.rahim@robertsoncollege.net>
2015-08-11 15:22:41 -0500

    database updated

--- Group_Project_Online_Exam/Instal.sql
+++ Group_Project_Online_Exam/Instal.sql
@@ -7,8 +7,6 @@ go
 use dbSD20
 go
 
------------------------------------------------
-
 create table tbSession
 (
 	SessionId int primary key identity(1,1),
@@ -16,6 +14,7 @@ create table tbSession
 )
 go
 
+	insert into tbSession (SessionCode) values ('SD20'),('AS')
 -----------------------------------------------
 
 create table tbProgram
@@ -26,13 +25,35 @@ create table tbProgram
 )
 go
 
-insert into tbProgram(ProgramName, SessionId)values
-	('Software and Database Developer', '1'),('Accounting Specialist', '2'),('Administrative Professional', '3'),
-	('Business Administration', '4'),('Casino / Resort / Event Coordinator', '5'),('Legal Assistant', '6'),
-	('Travel Counsellor', '7'),('Veterinary Office Assistant', '8'),('Network Engineering', '9'),
-	('Enhanced Health Care Aide', '10'),('Health Care Aide ', '11'),('Massage Therapy ', '12'),
-	('Medical Laboratory Assistant', '13'),('Medical Office Assistant', '14'),('Nursing Assistant', '15'),
-	('Pharmacy Technician', '16')
+-----------------------------------------------
+
+create table tbDifficulty
+(
+	DifficultyId int primary key identity(1,1),
+	Name Varchar(60)
+)
+go
+
+------------------------------------------------
+
+create table tbQuiz
+(
+  QuizeId int primary key identity(1,1),
+  ProgramId int foreign key references tbProgram(ProgramId),
+  DifficultyId int foreign key references tbDifficulty(DifficultyId)	     
+)
+go
+
+-----------------------------------------------
+
+insert into tbProgram(ProgramName,SessionId)values
+	('Software and Database Developer', 1),('Accounting Specialist', 2)
+	--('Administrative Professional', '3'),
+	--('Business Administration', '4'),('Casino / Resort / Event Coordinator', '5'),('Legal Assistant', '6'),
+	--('Travel Counsellor', '7'),('Veterinary Office Assistant', '8'),('Network Engineering', '9'),
+	--('Enhanced Health Care Aide', '10'),('Health Care Aide ', '11'),('Massage Therapy ', '12'),
+	--('Medical Laboratory Assistant', '13'),('Medical Office Assistant', '14'),('Nursing Assistant', '15'),
+	--('Pharmacy Technician', '16')
 go
 
 -----------------------------------------------
@@ -63,14 +84,6 @@ go
 
 -----------------------------------------------
 
-create table tbDifficulty
-(
-	DifficultyId int primary key identity(1,1),
-	Name Varchar(60)
-)
-go
-
------------------------------------------------
 create table tbQuestion
 (
   QuestionId int primary key identity(1,1),
@@ -84,34 +97,24 @@ create table tbQuestion
 )
 go
 
-------------------------------------------------
-create table tbQuestionResponse
-(
-  QuestionResponseId int primary key identity(1,1),
-  QuizResponseId int foreign key references tbQuizResponse(QuizResponseId),
-  QuestionId int foreign key references tbQuestion(QuestionId),
-  Response varchar(4)   
-)
-go
-
-------------------------------------------------
-
-create table tbQuiz
-(
-  QuizeId int primary key identity(1,1),
-  ProgramId int foreign key references tbProgram(ProgramId),
-  DifficultyId int foreign key references tbDifficulty(DifficultyId)	
-     
-)
-go
-
-------------------------------------------------
+-----------------------------------------------
 
 create table tbQuizResponse
 (
   QuizResponseId int primary key identity(1,1),
   ExamDate Date,
   StudentId int foreign key references tbStudent(StudentId)	    
+)
+go
+
+------------------------------------------------
+
+create table tbQuestionResponse
+(
+  QuestionResponseId int primary key identity(1,1),
+  QuizResponseId int foreign key references tbQuizResponse(QuizResponseId),
+  QuestionId int foreign key references tbQuestion(QuestionId),
+  Response varchar(4)   
 )
 go
 
@@ -136,3 +139,4 @@ create table tbActiveExam
 )
 go
 
+-----------------------------------------------
