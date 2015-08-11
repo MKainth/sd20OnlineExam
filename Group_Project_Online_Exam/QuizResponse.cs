aafc68a7f3ad4a303f08c63b65e80081f026088a
mohammad rahim <mohammad.rahim@robertsoncollege.net>
2015-08-11 16:56:19 -0500

    complete classes

---
+++ Group_Project_Online_Exam/QuizResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Group_Project_Online_Exam
+{
+    public class QuizResponse
+    {
+        public int QuizResponseId {get; set;}
+        public DateTime ExamDate {get; set;}
+        public int StudentId { get; set; }
+    }
+}
