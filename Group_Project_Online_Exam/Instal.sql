bc145b37bcdf185b9bfa7b30ebb670375042d2fa
Cristi Barroga <cristi.barroga@robertsoncollege.net>
2015-08-11 14:12:56 -0700

    "ghjg"

--- Group_Project_Online_Exam/Instal.sql
+++ Group_Project_Online_Exam/Instal.sql
@@ -38,4 +38,38 @@ create table tbStudent
 go
 
 -----------------------------------------------
+create table tbQuestion
+(
+  QuestionId int primary key identity(1,1),
+  Question varchar(500),
+  Answer1 varchar(50),
+  Answer2 varchar(50),
+  Answer3 varchar(50),
+  Answer4 varchar(50),
+  CorrectAnswer varchar(4),
+  Mark int
+)
+go
 
+------------------------------------------------
+create table tbQuestionResponse
+(
+  QuestionResponseId int primary key identity(1,1),
+  QuizResponseId int foreign key references tbQuizResponse(QuizResponseId),
+  QuestionId int foreign key references tbQuestion(QuestionId),
+  Response varchar(4)   
+)
+go
+
+----------------------------------------------------------
+create table tbActiveExam
+(
+  ActiveExamId int primary key identity(1,1),
+  QuizId int foreign key references tbQuiz(QuizId),
+  StartTime datetime,
+  EndTime datetime,
+  SessionId int foreign key references tbSession(SessionId)
+)
+go 
+
+------------------------------------------------------------
