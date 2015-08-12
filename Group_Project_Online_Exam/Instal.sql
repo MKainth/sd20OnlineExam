30f1dbd8395eab13e9a58601a018ce8db07213c9
MKainth <mandeep3183@gmail.com>
2015-08-12 16:13:12 -0500

    changes

--- Group_Project_Online_Exam/Instal.sql
+++ Group_Project_Online_Exam/Instal.sql
@@ -51,23 +51,6 @@ go
 go
 -----------------------------------------------
 
-create table tbProgram
-(
-	ProgramId int primary key identity(1,1),
-	ProgramName varchar(max),
-	SessionId int foreign key references tbSession(SessionId) 
-)
-go
-
-insert into tbProgram(ProgramName, SessionId)values
-	('Software and Database Developer', 1),('Accounting Specialist', 2)
-	--('Administrative Professional', '3'),
-	--('Business Administration', '4'),('Casino / Resort / Event Coordinator', '5'),('Legal Assistant', '6'),
-	--('Travel Counsellor', '7'),('Veterinary Office Assistant', '8'),('Network Engineering', '9'),
-	--('Enhanced Health Care Aide', '10'),('Health Care Aide ', '11'),('Massage Therapy ', '12'),
-	--('Medical Laboratory Assistant', '13'),('Medical Office Assistant', '14'),('Nursing Assistant', '15'),
-	--('Pharmacy Technician', '16')
-go
 -----------------------------------------------
 
 create table tbUser
