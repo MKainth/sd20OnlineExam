695603420afbf68ca16fa8401a5462ae60ddaa71
MKainth <mandeep3183@gmail.com>
2015-08-12 11:39:07 -0500

    hgj

--- Group_Project_Online_Exam/Instal.sql
+++ Group_Project_Online_Exam/Instal.sql
@@ -14,7 +14,7 @@ create table tbSession
 )
 go
 
-	--insert into tbSession (SessionCode) values ('SD20'),('AS')
+	insert into tbSession (SessionCode) values ('SD20'),('AS')
 -----------------------------------------------
 
 create table tbProgram
@@ -26,12 +26,12 @@ create table tbProgram
 go
 
 insert into tbProgram(ProgramName, SessionId)values
-	('Software and Database Developer', '1'),('Accounting Specialist', '2'),('Administrative Professional', '3'),
+	('Software and Database Developer', 1),('Accounting Specialist', 2)/*,('Administrative Professional', '3'),
 	('Business Administration', '4'),('Casino / Resort / Event Coordinator', '5'),('Legal Assistant', '6'),
 	('Travel Counsellor', '7'),('Veterinary Office Assistant', '8'),('Network Engineering', '9'),
 	('Enhanced Health Care Aide', '10'),('Health Care Aide ', '11'),('Massage Therapy ', '12'),
 	('Medical Laboratory Assistant', '13'),('Medical Office Assistant', '14'),('Nursing Assistant', '15'),
-	('Pharmacy Technician', '16')
+	('Pharmacy Technician', '16')*/
 go
 
 -----------------------------------------------
@@ -72,6 +72,12 @@ create table tbQuestion
   Answer4 varchar(50),
   CorrectAnswer varchar(4),
   Marks int
+)
+go
+create table tbDifficulty
+(
+	DifficultyId int primary key identity(1,1),
+	Name Varchar(60)
 )
 go
 
