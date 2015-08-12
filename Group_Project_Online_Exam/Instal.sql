d52a624d5bd6246a3cfc3e985d3071641d84ac3d
mohammad rahim <mohammad.rahim@robertsoncollege.net>
2015-08-12 12:43:15 -0500

    fgfg

--- Group_Project_Online_Exam/Instal.sql
+++ Group_Project_Online_Exam/Instal.sql
@@ -26,26 +26,27 @@ create table tbProgram
 go
 
 insert into tbProgram(ProgramName, SessionId)values
-	('Software and Database Developer', '1'),('Accounting Specialist', '2'),('Administrative Professional', '3'),
-	('Business Administration', '4'),('Casino / Resort / Event Coordinator', '5'),('Legal Assistant', '6'),
-	('Travel Counsellor', '7'),('Veterinary Office Assistant', '8'),('Network Engineering', '9'),
-	('Enhanced Health Care Aide', '10'),('Health Care Aide ', '11'),('Massage Therapy ', '12'),
-	('Medical Laboratory Assistant', '13'),('Medical Office Assistant', '14'),('Nursing Assistant', '15'),
-	('Pharmacy Technician', '16')
+	('Software and Database Developer', 1),('Accounting Specialist', 2)
+	--('Administrative Professional', '3'),
+	--('Business Administration', '4'),('Casino / Resort / Event Coordinator', '5'),('Legal Assistant', '6'),
+	--('Travel Counsellor', '7'),('Veterinary Office Assistant', '8'),('Network Engineering', '9'),
+	--('Enhanced Health Care Aide', '10'),('Health Care Aide ', '11'),('Massage Therapy ', '12'),
+	--('Medical Laboratory Assistant', '13'),('Medical Office Assistant', '14'),('Nursing Assistant', '15'),
+	--('Pharmacy Technician', '16')
 go
-
 -----------------------------------------------
 
-create table tbTeacher
+create table tbTeacher 
 (
 	TeacherId int primary key identity(1,1),
-	UserName Varchar(60),
+	TeacherName Varchar(60),
 	Password Varchar(60),
-	Email Varchar(50),
+	Email Varchar(50),---Email is use as UserName---
 	Admin int 
 )
 go
 
+--select * from tbTeacher
 -----------------------------------------------
 
 create table tbStudent
@@ -127,4 +128,73 @@ create table tbActiveExam
 )
 go
 
------------------------------------------------
+------------------------spInsertTeacher--------------------------
+
+go
+create proc spInsertTeacher
+(
+	@TeacherName Varchar(60),
+	@Password Varchar(60),
+	@Email Varchar(60),---Email is use as UserName---
+	@Admin int 
+)
+	as begin
+	if exists (select @TeacherName from tbTeacher where TeacherName=@TeacherName)
+	begin
+	select 'ERROR!' as Message
+		end
+		else begin
+			insert into tbTeacher(UserName,Password,Email,Admin)values
+			(@TeacherName,@Password,@Email,@Admin)
+			select 'OK' as Message
+end
+end
+
+---select * from tbTeacher---
+
+--------------------spGetTeacher---------------------------------
+go
+create proc spGetTeacher
+(
+	@TeacherId int=NULL
+)
+	as begin
+	Select TeacherId, TeacherName, Password, Email, Admin from tbTeacher
+	Where TeacherId=ISNULL (@TeacherId, TeacherId)
+end
+
+--------------------spDeleteTeacher-----------------------------
+
+go
+create proc spDeleteTeacher
+(
+	@TeacherId varchar (60)
+)
+	as begin
+	delete from tbTeacher
+	where TeacherId = @TeacherId
+end
+
+--exec spDeleteTeacher @TeacherId='Rahim'
+
+--------------------spUpdateTeacher-----------------------------
+
+go
+create proc spUpdateTeacher
+(
+	@TeacherId int,
+	@TeacherName Varchar(60),
+	@Password Varchar(60),
+	@Email Varchar(50),
+	@Admin int
+)
+	as begin update tbTeacher set TeacherName=@TeacherName,Passwrod=@Password, Email=@Email,Admin=@Admin
+	where teacherId=@TeacherId
+	end
+
+
+
+
+
+
+
