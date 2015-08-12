23e7f0c7bc78052757075348154d2f80039890f3
rimon.bishay <rimon.bishay@robertsoncollege.net>
2015-08-12 11:51:43 -0700

    Fixed Master page layout

--- Group_Project_Online_Exam/MasterPage.Master.designer.cs
+++ Group_Project_Online_Exam/MasterPage.Master.designer.cs
@@ -22,6 +22,15 @@ namespace Group_Project_Online_Exam {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
         /// h1Techer control.
         /// </summary>
         /// <remarks>
@@ -38,15 +47,6 @@ namespace Group_Project_Online_Exam {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink h1Student;
-        
-        /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
